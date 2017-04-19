94be5195c21a41c0adc928c94ebe66e8f0be694a
Kerrigan <kerrigan@glitchkitty.com>
2017-04-19 14:31:20 -0600

    Added Ex Nihilo Adscensio 0.1.20

--- git-pull-client.sh
+++ git-pull-client.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 echo Pulling Master Branch of KerriganMC
-git pull -b master https://github.com/Kavex/KerriganMC.git
+git reset --hard origin/master
 rm KerriganMC.zip
 zip -r KerriganMC.zip *
